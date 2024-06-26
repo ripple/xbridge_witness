65478cda0c022d1c4e8cbd889abf0588c7a6c3e1
Oleksandr <115580134+oleks-rip@users.noreply.github.com>
2024-06-26 10:34:33 -0400

    Version 1.1.0-rc1

--- src/xbwd/app/BuildInfo.cpp
+++ src/xbwd/app/BuildInfo.cpp
@@ -36,7 +36,7 @@ namespace build_info {
 //  and follow the format described at http://semver.org/
 //------------------------------------------------------------------------------
 // clang-format off
-char const* const versionString = "1.0.0"
+char const* const versionString = "1.1.0-rc1"
 // clang-format on
 
 #if defined(DEBUG) || defined(SANITIZER)
